2a46003e99aaf15fc9089c040265a21f3dd4dda8
Wojciech Kania <wojciech.kania@gmail.com>
2020-11-05 22:42:15 +0100

    [DoctrineBridge] Run Travis

--- src/Symfony/Bridge/Doctrine/Tests/Validator/Constraints/UniqueEntityValidatorTest.php
+++ src/Symfony/Bridge/Doctrine/Tests/Validator/Constraints/UniqueEntityValidatorTest.php
@@ -1132,7 +1132,6 @@ class UniqueEntityValidatorTest extends ConstraintValidatorTestCase
         $this->em->persist($entity);
         $this->em->flush();
 
-
         $dto = new UpdateCompositeObjectNoToStringIdEntity($objectOne, $objectTwo, 'Foo');
         $this->validator->validate($dto, $constraint);
     }
