8c02f9e5cc7c5ab99f7e31e31b93d6e23e93cfbf
Wojciech Kania <wojciech.kania@gmail.com>
2020-11-07 19:36:17 +0100

    [DoctrineBridge] Run Travis

--- src/Symfony/Component/Config/Tests/Resource/DirectoryResourceTest.php
+++ src/Symfony/Component/Config/Tests/Resource/DirectoryResourceTest.php
@@ -176,6 +176,7 @@ class DirectoryResourceTest extends TestCase
         $resourceA = new DirectoryResource($this->directory, '/.xml$/');
         $resourceB = new DirectoryResource($this->directory, '/.yaml$/');
 
+
         $this->assertCount(2, array_unique([$resourceA, $resourceB]));
     }
 }
